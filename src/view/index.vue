8b19f91197ac3317f1aa3c11430e9b5cdc9417e7
lmt <276977739@qq.com>
2018-04-17 18:22:38 +0800

    重整目录

---
+++ src/view/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="home">
+    <header id="header" class="">
+      <h1>标题</h1>
+    </header>
+    <main>
+    </main>
+    <footer>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data(){
+    return {
+      name:'zhangsan' 
+    }
+  },
+  methods:{
+   
+  }
+}
+</script>
+
+<style>
+
+</style>
