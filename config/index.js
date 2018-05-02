5e60ee43aa71af2333a71522c65bc48a12b06144
lmt <276977739@qq.com>
2018-05-02 14:17:11 +0800

    添加不提交代理文件

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ const path = require('path')
 module.exports = {
   dev: {
 
-    // Paths
+    // Paths configPath
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {},
